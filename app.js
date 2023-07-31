217b25f7ec30e2335c34e317ca98000144cb5901
Marina-yoya <64007447+Marina-yoya@users.noreply.github.com>
2023-07-31 19:51:52 +0100

    edit task

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 
 $(document).ready(function () {
 
+    const tasks = [];
+    let taskIndex = -1;
 
     $(".btn-add").click(function () {
         let inputValue = $("#task-input").val()
         let li = document.createElement("LI");
         li.textContent = inputValue;
         li.classList.add("list-group-item");
+
         let delBtn = document.createElement("button");
         delBtn.innerHTML = "Delete";
         delBtn.classList.add('btn', 'btn-outline-secondary', 'btn-del', 'px-2')
@@ -18,17 +21,38 @@ $(document).ready(function () {
         li.append(editBtn)
 
         $("#taskList").append(li);
+        tasks.push(li);
+        $("#task-input").val("");
 
-        
-        $(".btn-del").click(function(e){
+        $(".btn-del").click(function (e) {
             const li = e.target.parentNode;
             li.remove();
         });
-        
-        
+
+
+        $(".btn-edit").click(function (e) {
+            const editLi = e.target.parentElement.firstChild.textContent;
+            console.log(editLi, "li")
+            $("#task-edit").val(editLi);
+            taskIndex = $(this).parent().index();
+            console.log(taskIndex)
+        });
+
+        $(".btn-save").click(function (e) {
+            const editedTask = $("#task-edit").val();
+
+
+            if (taskIndex !== -1 && taskIndex < tasks.length) {
+                tasks[taskIndex] = editedTask;
+                $("#taskList li").eq(taskIndex).contents().get(0).nodeValue = editedTask
+            }
+
+            $("#task-edit").val("");
+            taskIndex = -1;
+        });
     });
-    
-
-
-
 });
+
+
+
+
